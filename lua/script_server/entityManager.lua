f1d8c64713d19707715428e6ea50bbcd132ff8b8
Marcelo Aragao e Silva Filho <marcelofilho1987@gmail.com>
2022-06-01 17:00:03 -0300

    Create Player, Fireball and Fire Rune

--- lua/script_server/entityManager.lua
+++ lua/script_server/entityManager.lua
@@ -3,5 +3,7 @@ local cfg = Entity.GetCfg('myplugin/player1')
 
 Trigger.RegisterHandler(cfg, "ENTITY_ENTER", function(context)
     local player = context.obj1
+    player:addItem('myplugin/e18a4eb5-f32f-43f3-bd95-0f0de3839a42', 1, nil, 'gift')
+    player:addItem('myplugin/fireRune', 10, nil, 'gift')
     Lib.emitEvent('PLAYER_ENTER', player)
 end)
